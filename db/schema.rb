eb25e285f697e53abc9a4a9b72f9a5a5d8d1fa7b
Sagar <sarg@outlook.com>
2022-04-11 03:01:59 +0100

    Update Recipe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_220940) do
+ActiveRecord::Schema.define(version: 2022_04_11_003616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_04_10_220940) do
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "ingredients", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|
